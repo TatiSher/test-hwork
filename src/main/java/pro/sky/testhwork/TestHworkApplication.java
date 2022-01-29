9da4237b63f3f6f58d871e4eb7a34218d317fa20
Tatiana <tanja6122212@list.ru>
2022-01-30 00:11:23 +0900

    Initial commit

---
+++ src/main/java/pro/sky/testhwork/TestHworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testhwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/testhwork/TestHworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.testhwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
